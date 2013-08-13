fb7d3196b50643b0650221b72a6f6f380b38e313
Joe <josschne@gmail.com>
2013-08-13 01:17:21 -0500

    Add script to test on EC2

--- Rakefile
+++ Rakefile
@@ -2,13 +2,19 @@ task :default => [:identify]
 
 depends_dictionary = {'SDL.h'=> 'libsdl-dev'}
 
+require 'iconv'
+
 task :detect_third_party_dependencies do
 	puts "Detect third party dependencies"
 	require 'yaml'
 	Dir['**/*.h', '**/*.cpp'].each do |file|
 		depends_dictionary.each do |signature, lib|
-			if not File.directory?(file) and open(file).grep(/#{signature}/i).length > 0
-				puts "Found dependency #{lib}"
+			if not File.directory?(file)
+				ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+				contents = ic.iconv(open(file).read)
+				if contents.split('\n').grep(/#{signature}/i).length > 0
+					puts "Found dependency #{lib}"
+				end
 			end
 		end
 	end
@@ -23,7 +29,11 @@ task :detect_build_command do
 		puts "Found automake build"
 		sh "mkdir -p build && cd build && ../configure && make clean && make && make test"
 	elsif File.exists?('SConscript')
+		puts "Found Scons build"
 		sh "scons -c && scons"
+	elsif Dir['*.pro'].length > 0
+		puts "Found QMake build"
+		sh "qmake #{Dir['*.pro'][0]} -o Makefile && make"
 	end
 end
 
---
+++ randomGitTest.rb
@@ -0,0 +1,60 @@
+require 'aws-sdk'    
+require 'github_api' #Use version 0.10.1 to avoid nokogiri conflicts for now
+require 'net/ssh'
+
+begin
+
+def getRandomRepo()
+	randomRepoUrl = ""
+	search = Github::Search.new
+	result = search.repos keyword: 'a', language: 'c++'
+	if result.repositories.length > 0
+		randomRepoUrl = result.repositories[rand(result.repositories.length)].url
+		puts "Repo: #{randomRepoUrl}"
+	end
+	return randomRepoUrl
+end
+
+def launchEC2()
+	ec2 = AWS::EC2.new
+
+	if not ec2.key_pairs["randomGitHubTest"].exists?
+		key_pair = ec2.key_pairs.create("randomGitHubTest")
+		File.open("#{ENV['HOME']}/.ssh/randomGitHubTest", "wb") do |f|
+	  		f.write(key_pair.private_key)
+		end
+	end
+
+	#instance = ec2.instances.create(:image_id=>'ami-a1d4a2c8', :key_name=>'randomGitHubTest', :instance_type=>'t1.micro')
+	instance = ec2.instances['i-87495ce8']
+	puts "Waiting for instance to transition to running state..."
+	sleep 10 while instance.status == :pending
+	puts "Instance state: #{instance.status}"
+	return instance
+end
+
+def installPrereqs(instance, repo)
+	puts "Public DNS: #{instance.dns_name}"
+	Net::SSH.start(instance.dns_name, 'ubuntu', :keys=>["#{ENV['HOME']}/.ssh/randomGitHubTest"]) do |session|
+		puts "Successfully connected over SSH!"
+		puts session.exec!("export DEBIAN_FRONTEND=noninteractive")
+		puts session.exec!("sudo apt-get update -y")
+		puts session.exec!("sudo apt-get install -y git build-essential")
+		puts session.exec!("git clone https://github.com/josschne/UltimateMakefile.git")
+		puts session.exec!("git clone #{repo}")
+		puts session.exec!("cd #{`basename repo`} && ../UltimateMakefile/install.sh && rake")
+	end
+end
+
+def terminateEC2(instance)
+	puts "Terminating instance"
+	#instance.terminate
+	puts "Done"
+end
+
+randomRepo = getRandomRepo
+instance = launchEC2
+installPrereqs(instance, randomRepo)
+terminateEC2(instance)
+
+end
